34595ed879ce6093844de590a2328033931dc57b
Aravind <aravindakumar1999@gmail.com>
2022-08-04 17:43:42 +0530

    Created Sign Up Page and Checking conditions of fields before connect to Firebase

--- app/src/main/java/com/example/loginactivity/SignUp.java
+++ app/src/main/java/com/example/loginactivity/SignUp.java
@@ -1,6 +1,7 @@
 package com.example.loginactivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +15,6 @@ import com.google.firebase.auth.FirebaseAuth;
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
     private TextView signupbtn;
     private EditText editName, editEmail, editPassword;
-    private ProgressBar progressBar;
 
     private FirebaseAuth mAuth; //declare and initialize our Auth
     @Override
@@ -36,6 +36,38 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v)
     {
-
+        String name=editName.getText().toString().trim();
+        String email=editEmail.getText().toString().trim();
+        String password=editPassword.getText().toString().trim();
+        if(name.isEmpty())
+        {
+            editName.setError("Enter Name");
+            editName.requestFocus();
+            return;
+        }
+        if(email.isEmpty())
+        {
+            editEmail.setError("Enter Email Id");
+            editEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty())
+        {
+            editPassword.setError("Enter Password");
+            editPassword.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            editEmail.setError("Please provide valid email");
+            editEmail.requestFocus();
+            return;
+        }
+        if(password.length() < 6)
+        {
+            editPassword.setError("Minimum password length should be 6");
+            editPassword.requestFocus();
+            return;
+        }
     }
 }
