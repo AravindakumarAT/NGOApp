dfc53d3bc658291d03e5a40618e34272afd2c114
Aravind <aravindakumar1999@gmail.com>
2022-08-04 14:27:36 +0530

    Firebase Dependencies added and App connected to Firebase

--- app/src/main/java/com/example/loginactivity/MainActivity.java
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.loginactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,31 +10,30 @@ import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView register;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username =(TextView) findViewById(R.id.email);
-        TextView password =(TextView) findViewById(R.id.password);
-
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    //correct
-                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-                }else
-                    //incorrect
-                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
+        register=(TextView) findViewById(R.id.signup);
+        register.setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.signup: startActivity(new Intent(this,SignUp.class));
+            break;
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/loginactivity/SignUp.java
+++ app/src/main/java/com/example/loginactivity/SignUp.java
@@ -1,4 +1,15 @@
 package com.example.loginactivity;
 
-public class SignUp {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstancesState)
+    {
+        super.onCreate(savedInstancesState);
+        setContentView(R.layout.signup);
+    }
 }
